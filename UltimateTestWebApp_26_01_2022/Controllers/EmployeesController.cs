d03ea63dc031fad32adf3edcf054f246de57ef16
Denis ALeksandrov <denis_aleksandrov_12@mail.ru>
2022-01-27 21:21:27 +0300

    /

--- UltimateTestWebApp_26_01_2022/Controllers/EmployeesController.cs
+++ UltimateTestWebApp_26_01_2022/Controllers/EmployeesController.cs
@@ -6,8 +6,10 @@ using AutoMapper;
 using Contracts;
 using Entities.Dto;
 using Entities.Models;
+using Entities.RequestFeatures;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Api.Controllers
 {
@@ -27,16 +29,20 @@ namespace Api.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetEmployeesForCompany(Guid companyId)
+        public async Task<IActionResult> GetEmployeesForCompany(Guid companyId,
+            [FromQuery] EmployeeParameters employeeParametrs)
         {
-            var company = _repository.Company.GetCompanyAsync(companyId, trackChanges: false);
+            var company = await _repository.Company.GetCompanyAsync(companyId, trackChanges: false);
             if (company == null)
             {
                 _logger.LogInfo($"Company with id: {companyId} doesn't exist in the database.");
                 return NoContent();
             }
+
             var employeesFromDb = await _repository.Employee.GetEmployeesAsync(companyId,
-                trackChanges: false);
+                employeeParametrs, trackChanges: false);
+
+            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(employeesFromDb.MetaData));
 
             var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesFromDb);
 
